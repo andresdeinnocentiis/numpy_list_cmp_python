a9bb4c8f7df7e56534498f9eeb1332b2034bc446
Andres De Innocentiis <79876962+andresdeinnocentiis@users.noreply.github.com>
2021-09-19 13:08:08 -0300

    Update ejercicio_4.py

--- ejercicios_practica/ejercicio_4.py
+++ ejercicios_practica/ejercicio_4.py
@@ -25,10 +25,30 @@ if __name__ == '__main__':
     # TIP: Recomendamos ver el método "isdigit" de strings
     # para aplicar en este caso.
     list_numeros_str = ['5', '2', '3', '', '7', 'NaN']
-
+    
+    lista_digit = [int(x) if x.isdigit() else 0 for x in list_numeros_str]
+    print(lista_digit)
 
     # ¿Ya terminaron el ejercicio? ¿Por qué no prueban
     # hacer negativo alguno de los números de la lista?
     # ¿Qué sucede con isdigit? Sorprendente no?
+    
+    list_numeros_str2 = ['5', '2', '-3', '', '7', 'NaN']
+    lista_digit2 = [int(x) if x.isdigit() else 0 for x in list_numeros_str2]
+    print(lista_digit2)
+        
+    def is_numero(num):
+        numero = False
+        try: 
+            int(num)
+            numero = True
+        except ValueError:
+            numero = False #Sé que no es buena práctica manejar una excepción así pero para la finalidad de la función no necesitaba más.
+        return numero
+      
+    list_numeros_str3 = ['5', '2', '-3', '', '7', 'NaN']            
+            
+    lista_numeric = [int(x) if is_numero(x) == True else 0 for x in list_numeros_str3]
+    print(lista_numeric)
 
     print("terminamos")
